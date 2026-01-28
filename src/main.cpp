082151d6be9af427dd7d451c4d5fdeb89f8d9ea5
Evan Suma Rosenberg <suma@umn.edu>
2026-01-28 13:19:39 -0600

    pre-class commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/* Programming 1: Tools of the Trade
+ * CSCI 5980, Spring 2026, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+
+	return EXIT_SUCCESS;
+}
